d47e3a6b38b06108de0d9b7c33a57620e83484e5
Mikhail Tkachev <tritium.mkl@gmail.com>
2024-09-26 10:42:25 +0300

    Add project.

---
+++ cities.js
@@ -0,0 +1,56 @@
+const cities = [
+  {
+    label: "Krasnodar",
+    value: "1",
+  },
+  {
+    label: "Moscow",
+    value: "2",
+  },
+  {
+    label: "Sochi",
+    value: "3",
+  },
+  {
+    label: "Gelenjic",
+    value: "4",
+  },
+  {
+    label: "Orel",
+    value: "5",
+  },
+  {
+    label: "Samara",
+    value: "6",
+  },
+  {
+    label: "EKB",
+    value: "7",
+  },
+  {
+    label: "SSS",
+    value: "8",
+  },
+  {
+    label: "DDD",
+    value: "9",
+  },
+  {
+    label: "AAA",
+    value: "10",
+  },
+  {
+    label: "UUU",
+    value: "11",
+  },
+  {
+    label: "KKK",
+    value: "12",
+  },
+  {
+    label: "GGG",
+    value: "13",
+  },
+];
+
+module.exports = cities;
